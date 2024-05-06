b8cb946a0503823188ff124160ded9936e3eea3b
Amber <Amberorano1246@gmail.com>
2024-05-06 11:49:18 +0800

    l

--- main_app/forms.py
+++ main_app/forms.py
@@ -67,13 +67,6 @@ class StudentForm(CustomUserForm):
     state = forms.ChoiceField(choices=[('Currently Learning', _('Currently Learning')), ('Completed', _('Completed')), ('Refund', _('Refund'))], label=_("State"), widget=forms.Select(attrs={'class': 'hideable'}))
     
     # Include new fields: campus, institution, grade, home_number, cell_number
-<<<<<<< HEAD
-    institution = forms.ModelChoiceField(queryset=Institution.objects.all(), required=False, label=_("Institution"), widget=forms.TextInput(attrs={'class': 'hideable'}))
-    campus = forms.ModelChoiceField(queryset=Campus.objects.all(), required=False, label=_("Campus"), widget=forms.TextInput(attrs={'class': 'hideable'}))
-    grade = forms.CharField(max_length=10, required=False, label=_("Grade"), widget=forms.TextInput(attrs={'class': 'hideable'}))
-    home_number = forms.CharField(max_length=20, required=False, label=_("Home Number"), widget=forms.TextInput(attrs={'class': 'hideable'}))
-    cell_number = forms.CharField(max_length=20, required=False, label=_("Cell Number"), widget=forms.TextInput(attrs={'class': 'hideable'}))
-=======
     institution = forms.ModelChoiceField(queryset=Institution.objects.all(), required=False, label=_("Institution"))
     campus = forms.ModelChoiceField(queryset=Campus.objects.all(), required=False, label=_("Campus"))
     grade = forms.CharField(max_length=10, required=False, label=_("Grade"))
@@ -81,7 +74,6 @@ class StudentForm(CustomUserForm):
     cell_number = forms.CharField(max_length=20, required=False, label=_("Cell Number"))
     course = forms.ModelChoiceField(queryset=Course.objects.all(), required=False, label=_("Course"))
     session = forms.ModelChoiceField(queryset=Session.objects.all(), required=False, label=_("Session"))
->>>>>>> b12215f25d0989ba2f17eefefb6f9eb6c770dbad
 
     def __init__(self, *args, **kwargs):
         super(StudentForm, self).__init__(*args, **kwargs)
